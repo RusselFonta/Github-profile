ed34b583a0a8e309328b86e3e1d66fab5ee68d15
Russel Fonta <fontawestbrook99@gmail.com>
2026-09-02 11:14:56 +0200

    feat : fetch and extraction of the json()

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-const ProfileName = document.querySelector('.Input_bar');
-const SearchBtn = document.querySelector('.Search_btn');
+const ProfileName = document.querySelector('.Input_bar')
+const SearchBtn = document.querySelector('.Search_btn')
+const Avatar = document.querySelector('.Avatar')
 
-const ProfileSearch = () => {
+const ProfileSearch = async () => {
   try {
-    const input = ProfileName.value.trim();
-    if (input === "") {
-      alert("Enter a username to search");
+    const username = ProfileName.value.trim()
+
+    if (username === "") {
+      alert("Enter a username to search")
       return;
     }
-  } catch {}
+
+    const url = `https://api.github.com/users/${username}`;
+    const data = await fetch(url);
+
+    if (!data.ok) {
+      if (data.status === 404) {
+        alert(`${username} not found`);
+        return;
+      }
+      throw new Error(`Response Status : ${data.status}`);
+    }
+
+    const response = await data.json();
+    console.log(response);
+
+
+
+  } catch (error) {
+    alert(error.message);
+    console.log(error.message);
+  }
 };
 
 SearchBtn.addEventListener("click", ProfileSearch);
